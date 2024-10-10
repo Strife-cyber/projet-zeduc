278a108db01ae75fe6e7048bd9f66bbca17821e4
Djiatsa Dunamis <dunamisjunior15@gmail.com>
2024-10-10 15:53:23 +0100

    Login updated and complete

--- index.php
+++ index.php
@@ -2,5 +2,16 @@
 
 require_once 'routes.php';
 
-header("Content-Type: application/json"); // Set the response type to JSON
-routeRequest(); // Call the routing function
+// Ajoutez les en-têtes CORS
+header("Access-Control-Allow-Origin: http://localhost:3000"); // Remplacez par l'origine de votre application React
+header("Access-Control-Allow-Methods: POST, OPTIONS"); // Méthodes autorisées
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // En-têtes autorisés
+
+// Gestion des requêtes OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit(); // Répondez immédiatement aux requêtes OPTIONS
+}
+
+header("Content-Type: application/json"); // Définir le type de réponse sur JSON
+routeRequest(); // Appelez la fonction de routage
